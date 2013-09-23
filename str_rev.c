963e41bbbde274f8ec4fc4a6cadbc6ee9dce365d
teguhginanjar <tghgnjr@gmail.com>
2013-09-23 16:02:27 +0700

    Create str_rev.c
    string rev

---
+++ str_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char * rev_str (char * str)
+{
+	char * tmp = (char * ) malloc (sizeof (char ));
+	int len = strlen (str) -1;
+	tmp[len] = '\0';
+	do {
+		tmp[len--] = *str;
+	} while (*str++ != '\0');
+	return tmp;
+}
+
+
+int main (int argc, char ** argv)
+{
+	if (argc < 2) return 1;
+	char * revstr = rev_str (argv[1]);
+	puts (revstr);
+	free (revstr);
+  return 0;
+}
